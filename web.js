34f11b2d89775f8e3c0a41246fc457a0fd3b484b
Zach Rose <zwrose@gmail.com>
2014-01-03 14:38:15 -0500

    adding some 'excitement'

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Love from zwrose!');
+  response.send('Hello World! Love from zwrose! Cannot wait to have some beautiful content up here!');
 });
 
 var port = process.env.PORT || 5000;
