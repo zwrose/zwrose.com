d94b4aee88df4ca4776f381e3ad99a0797863ac2
Zach Rose <zwrose@gmail.com>
2014-01-03 14:03:57 -0500

    love from zwrose added

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! Love from zwrose!');
 });
 
 var port = process.env.PORT || 5000;
