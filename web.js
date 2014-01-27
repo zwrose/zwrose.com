22a6adc35abd06758a7f8c7066ae05f027a77cfb
Zach Rose <zwrose@gmail.com>
2014-01-26 22:49:21 -0500

    added poet to json, beginning to update web.js to include poet

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var poet = require('poet')(app);
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World! Love from zwrose! Cannot wait to have some beautiful content up here!');
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World! Love from zwrose! Cannot wait to have some beautiful content up here!');
 });
 
 var port = process.env.PORT || 5000;
