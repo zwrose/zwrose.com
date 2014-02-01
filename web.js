06bd0e9bae96ebb580818060a4d878a925b88c22
Zach Rose <zwrose@gmail.com>
2014-01-31 22:32:28 -0500

    poet is in under /blolog

--- web.js
+++ web.js
@@ -1,15 +1,28 @@
-var express = require('express');
-var app = express();
-var poet = require('poet')(app);
-app.use(express.logger());
+var
+  express = require('express'),
+  app = express(),
+  Poet = require('poet');
 
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
+var poet = Poet(app, {
+  posts: './_posts/',
+  postsPerPage: 5,
+  metaFormat: 'json'
+});
 
+poet.init().then(function () {
+  // ready to go!
+});
+
+app.set( 'view engine', 'jade' );
+app.set( 'views', __dirname + '/views' );
+app.use( express.static( __dirname + '/public' ));
+app.use( app.router );
 
 app.get('/', function(req, res) {
   res.send('Hello World! Love from zwrose! Cannot wait to have some beautiful content up here!');
 });
+
+app.get( '/blog', function ( req, res ) { res.render( 'index' ) });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
